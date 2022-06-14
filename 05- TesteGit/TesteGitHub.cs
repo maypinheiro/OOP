59b675096029e757b91cfd7fec5f9fa6ecc50143
maypinheiro <mayarapinheirodesousa@hotmail.com>
2022-06-14 11:15:16 -0300

    Okay

---
+++ 05- TesteGit/TesteGitHub.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OOP 
+{
+    public class TesteGitHub
+    {
+        public string Nome { get; set; }
+        public string Tipo { get; set; }
+    }
+}
